00e6f42300b60aa80b3436d9dd8dddb3681d89ff
tony wu <tony.wu@outlook.com>
2014-12-14 17:00:13 +0800

    u

---
+++ Cosmoser.PingAnMeetingRequest.Outlook2010/Views/IMobileTermView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cosmoser.PingAnMeetingRequest.Common.Model;
+using System.Windows.Forms;
+
+namespace Cosmoser.PingAnMeetingRequest.Outlook2010.Views
+{
+    public interface IMobileTermView
+    {
+        List<MobileTerm> MobileTermList { get; set; }
+        DialogResult Display();
+    }
+}
